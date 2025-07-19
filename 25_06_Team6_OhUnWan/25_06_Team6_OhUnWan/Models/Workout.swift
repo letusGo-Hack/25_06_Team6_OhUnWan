85307441e1d94c963d1ebbdcb7ddf3bb7297caad
JuseongMoon <37950716+JuseongMoon@users.noreply.github.com>
2025-07-19 15:01:31 +0900

    Revert "한글화작업"
    This reverts commit 5263873ca5c8f5c1c8bf335c023f15fd990dc305.

--- 25_06_Team6_OhUnWan/25_06_Team6_OhUnWan/Models/Workout.swift
+++ 25_06_Team6_OhUnWan/25_06_Team6_OhUnWan/Models/Workout.swift
@@ -16,22 +16,9 @@ struct Workout: Identifiable {
     let caloriesBurned: Double?
 
     var formattedDuration: String {
-        let hours = Int(duration) / 3600
-        let minutes = Int(duration) % 3600 / 60
-        let seconds = Int(duration) % 60
-        
-        var result = ""
-        
-        if hours > 0 {
-            result += "\(hours)시간 "
-        }
-        if minutes > 0 {
-            result += "\(minutes)분 "
-        }
-        if seconds > 0 || (hours == 0 && minutes == 0) {
-            result += "\(seconds)초"
-        }
-        
-        return result.trimmingCharacters(in: .whitespaces)
+        let formatter = DateComponentsFormatter()
+        formatter.allowedUnits = [.hour, .minute, .second]
+        formatter.unitsStyle = .abbreviated
+        return formatter.string(from: duration) ?? ""
     }
 }
