02d4950ef5f902b79f6919b5018ff90b6aa5c082
JuseongMoon <37950716+JuseongMoon@users.noreply.github.com>
2025-07-19 09:56:56 +0900

    프로젝트 생성

---
+++ 25_06_Team6_OhUnWan/25_06_Team6_OhUnWan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  25_06_Team6_OhUnWan
+//
+//  Created by 문주성 on 7/19/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 25_06_Team6_OhUnWan/25_06_Team6_OhUnWan/_5_06_Team6_OhUnWanApp.swift
@@ -0,0 +1,17 @@
+//
+//  _5_06_Team6_OhUnWanApp.swift
+//  25_06_Team6_OhUnWan
+//
+//  Created by 문주성 on 7/19/25.
+//
+
+import SwiftUI
+
+@main
+struct _5_06_Team6_OhUnWanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
